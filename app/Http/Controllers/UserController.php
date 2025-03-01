28bb59f2cd0cd27be696d6764c856c7e013066ec
2S SIN Waldenylson <waldenylsonwpss@fab.mil.br>
2025-03-01 07:17:41 -0300

    refactor: improve code formatting and readability in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,109 +8,108 @@ use Illuminate\Auth\Events\Registered;
 
 class UserController extends Controller
 {
-    private UserRepository $usersRepository;
+  private UserRepository $usersRepository;
 
-    public function __construct(UserRepository $repository)
-    {
-        $this->usersRepository = $repository;
+  public function __construct(UserRepository $repository)
+  {
+    $this->usersRepository = $repository;
+  }
+
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $usuarios = $this->usersRepository->listUsers();
+
+    return view('users.index')->with(compact('usuarios'));
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //return redirect()->back()->with('error', 'Erro ao Tentar Inserir o Registro!');
+
+    return view('users.new');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreUsersPostRequest $request)
+  {
+    $this->checkEmailFAB($request);
+
+    $result = $this->usersRepository->store($request);
+
+    if ($result) {
+      event(new Registered($result));
+
+      return redirect()->back()->with('message', 'Registro Inserido com Sucesso!');
     }
 
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $usuarios = $this->usersRepository->listUsers();
+    return redirect()->back()->with('error', 'Erro ao Tentar Inserir o Registro!');
+  }
 
-        return view('users.index')->with(compact('usuarios'));
+  /**
+   * Display the specified resource.
+   */
+  public function show(int $id)
+  {
+    //
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit($id)
+  {
+    $user = $this->usersRepository->edit($id);
+
+    return view('users.edit', compact('user'));
+  }
+
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(StoreUsersPostRequest $request, $id)
+  {
+    $this->checkEmailFAB($request);
+
+    $result = $this->usersRepository->persistUpdate($request, $id);
+
+    if ($result) {
+      return redirect()->back()->with('message', 'Registro Alterado com Sucesso!');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //return redirect()->back()->with('error', 'Erro ao Tentar Inserir o Registro!');
+    return redirect()->back()->with('error', 'Erro ao Tentar Alterar o Registro!');
+  }
 
-        return view('users.new');
+  /**
+   * Remove um registro especifico do Banco.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    $result = $this->usersRepository->destroy($id);
+
+    if ($result) {
+      return redirect()->back()->with('message', 'Registro Removido com Sucesso!');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreUsersPostRequest $request)
-    {
-        $this->checkEmailFAB($request);
+    return redirect()->back()->with('error', 'Erro ao Tentar Remover o Registro!');
+  }
 
-        $result = $this->usersRepository->store($request);
+  private function checkEmailFAB($request)
+  {
+    $dominio = explode("@", $request['email']);
 
-        if ($result) {
-            event(new Registered($result));
-
-            return redirect()->back()->with('message', 'Registro Inserido com Sucesso!');
-        }
-
-        return redirect()->back()->with('error', 'Erro ao Tentar Inserir o Registro!');
+    if ($dominio[1] !== "fab.mil.br") {
+      return redirect()->back()->withErrors("E-Mail FAB Obrigatório!")->withInput();
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(int $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        $user = $this->usersRepository->edit($id);
-
-        return view('users.edit', compact('user'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(StoreUsersPostRequest $request, $id)
-    {
-        $this->checkEmailFAB($request);
-
-        $result = $this->usersRepository->persistUpdate($request, $id);
-
-        if ($result) {
-            return redirect()->back()->with('message', 'Registro Alterado com Sucesso!');
-        }
-
-        return redirect()->back()->with('error', 'Erro ao Tentar Alterar o Registro!');
-
-    }
-
-    /**
-     * Remove um registro especifico do Banco.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        $result = $this->usersRepository->destroy($id);
-
-        if ($result) {
-            return redirect()->back()->with('message', 'Registro Removido com Sucesso!');
-        }
-
-        return redirect()->back()->with('error', 'Erro ao Tentar Remover o Registro!');
-    }
-
-    private function checkEmailFAB($request)
-    {
-        $dominio = explode("@", $request['email']);
-
-        if ($dominio[1] !== "fab.mil.br") {
-            return redirect()->back()->withErrors("E-Mail FAB Obrigatório!")->withInput();
-        }
-    }
+  }
 }
