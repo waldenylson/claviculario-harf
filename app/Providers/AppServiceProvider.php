48fcc851d24b0ec35b89d64ff2ee96ee5e385c3e
2S SIN Waldenylson <waldenylsonwpss@fab.mil.br>
2025-03-02 08:55:36 -0300

    refactor: improve code structure and readability in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,54 +10,55 @@ use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
 {
-    private $keysCount;
-    private $outKeysCount;
+  private $keysCount;
+  private $outKeysCount;
 
-    public function __construct() {
-        try {
-            $this->keysCount = DB::table('keys')->count();
-            $this->outKeysCount = DB::table('key_movements')->count();
-        } catch (\Throwable $th) {
-            $this->keysCount = 0;
-            $this->outKeysCount = 0;
-        }
+  public function __construct()
+  {
+    try {
+      $this->keysCount = DB::table('keys')->count();
+      $this->outKeysCount = DB::table('key_movements')->count();
+    } catch (\Throwable $th) {
+      $this->keysCount = 0;
+      $this->outKeysCount = 0;
     }
+  }
 
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(Dispatcher $events): void
-    {
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(Dispatcher $events): void
+  {
 
 
-        $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
-            $event->menu->addAfter('claviculario', [
-                'text' => 'Chaves Retiradas',
-                'url'  => '#',
-                'icon' => 'fa-solid fa-list-check',
-                'icon_color' => 'red',
-                'label'       => $this->outKeysCount,
-                'label_color' => 'danger'
-            ]);
+    $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+      $event->menu->addAfter('claviculario', [
+        'text' => 'Chaves Retiradas',
+        'url'  => '#',
+        'icon' => 'fa-solid fa-list-check',
+        'icon_color' => 'red',
+        'label'       => $this->outKeysCount,
+        'label_color' => 'danger'
+      ]);
 
-            $event->menu->addAfter('claviculario', [
-                'text' => 'Total Chaves',
-                'id' => 'total_chaves',
-                'url'  => '#',
-                'icon' => 'fa-solid fa-list',
-                'icon_color' => '',
-                'label'       => $this->keysCount,
-                'label_color' => 'primary',
-                'update_class_name' => 'total-chaves'
-            ]);
-        });
-    }
+      $event->menu->addAfter('claviculario', [
+        'text' => 'Total Chaves',
+        'id' => 'total_chaves',
+        'url'  => '#',
+        'icon' => 'fa-solid fa-list',
+        'icon_color' => '',
+        'label'       => $this->keysCount,
+        'label_color' => 'primary',
+        'update_class_name' => 'total-chaves'
+      ]);
+    });
+  }
 }
