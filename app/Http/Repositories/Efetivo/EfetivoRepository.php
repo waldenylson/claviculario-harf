aa52c663e3a279e81cc5bd21a253d0f5f824d15b
Waldenylson Silva <waldenylson@gmail.com>
2025-03-03 16:23:49 -0300

    refactor: improve structure and readability in EfetivoRepository and enhance form layout in the view

--- app/Http/Repositories/Efetivo/EfetivoRepository.php
+++ app/Http/Repositories/Efetivo/EfetivoRepository.php
@@ -8,67 +8,67 @@ use Illuminate\Support\Facades\Hash;
 
 class EfetivoRepository implements EfetivoRepositoryContract
 {
+  protected string $modelClass = HarfStaff::class;
 
+  public function listStaff(): \Illuminate\Database\Eloquent\Collection
+  {
+    return $this->modelClass::all();
+  }
 
-    protected string $modelClass = HarfStaff::class;
+  public function findSingleStaff($id)
+  {
+    return $this->modelClass::findOrFail($id);
+  }
 
-    public function listStaff(): \Illuminate\Database\Eloquent\Collection
-    {
-        return $this->modelClass::all();
+  public function store(StoreEfetivoPostRequest $request)
+  {
+    dd($request);
+
+    return $this->modelClass::create($request->all());
+  }
+
+  public function edit($id)
+  {
+    return $this->modelClass::findOrFail($id);
+  }
+
+  public function persistUpdate(StoreEfetivoPostRequest $request, $id)
+  {
+    $validatedData = $request->validated();
+
+    if ($request->filled('electronic_signature')) {
+      $validatedData['electronic_signature'] = Hash::make($request->electronic_signature);
+    } else {
+      unset($validatedData['electronic_signature']);
     }
 
-    public function findSingleStaff($id)
-    {
-        return $this->modelClass::findOrFail($id);
-    }
+    return $this->modelClass::findOrFail($id)->update($validatedData);
+  }
 
-    public function store(StoreEfetivoPostRequest $request)
-    {
-        return $this->modelClass::create($request->all());
-    }
+  public function destroy($id)
+  {
+    $user = $this->modelClass::findOrFail($id);
 
-    public function edit($id)
-    {
-        return $this->modelClass::findOrFail($id);
-    }
+    return $user->delete();
+  }
 
-    public function persistUpdate(StoreEfetivoPostRequest $request, $id)
-    {
-        $validatedData = $request->validated();
+  public function index()
+  {
+    // Implement the index method
+  }
 
-        if ($request->filled('electronic_signature')) {
-            $validatedData['electronic_signature'] = Hash::make($request->electronic_signature);
-        } else {
-            unset($validatedData['electronic_signature']);
-        }
+  public function create()
+  {
+    // Implement the create method
+  }
 
-        return $this->modelClass::findOrFail($id)->update($validatedData);
-    }
+  public function show($id)
+  {
+    // Implement the show method
+  }
 
-    public function destroy($id)
-    {
-        $user = $this->modelClass::findOrFail($id);
-
-        return $user->delete();
-    }
-
-    public function index()
-    {
-        // Implement the index method
-    }
-
-    public function create()
-    {
-        // Implement the create method
-    }
-
-    public function show($id)
-    {
-        // Implement the show method
-    }
-
-    public function getAllUsersForSelect()
-    {
-        // Implement the getAllUsersForSelect method
-    }
+  public function getAllUsersForSelect()
+  {
+    // Implement the getAllUsersForSelect method
+  }
 }
--- resources/views/efetivo/partials/form.blade.php
+++ resources/views/efetivo/partials/form.blade.php
@@ -1,20 +1,40 @@
 {{ $editObjectInstance = isset($featureInstance) ? $featureInstance : null }}
 
-{{dd($editObjectInstance)}}
+{{ isset($editObjectInstance) ? dd($editObjectInstance) : '' }}
 
 <x-AppComponents::form-template :featureInstance="$editObjectInstance">
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-2">
+      {{-- <label for="military" class="form-label label">Militar</label> --}}
+      <div class="flex items-center">
+        <input type="checkbox" id="military" name="military" value=""
+          class="form-checkbox h-5 w-5 text-gray-700 dark:text-gray-300
+           dark:bg-gray-800 dark:border-gray-600 focus:ring-0 focus:ring-offset-0"
+          {{ old('military', $editObjectInstance->military ?? false) ? 'checked' : '' }} />
+        <label for="military" class=" form-label label">Militar</label>
+      </div>
+    </div>
+  </div><br />
+  <div class="row">
+    <div class="col-md-3">
       <label for="departament_id" class="form-label label">Departamento</label>
       <select class="form-control" id="departament_id" name="departament_id" required>
+        <option value="">Selecione...</option>
+        <option value="0">Sem Departamento</option>
         @foreach ($departments as $department)
           <option value="{{ $department->id }}">{{ $department->name }}</option>
         @endforeach
       </select>
     </div>
-    <div class="col-md-4">
-      <label for="name" class="form-label label">Nome</label>
-      <input type="text" class="form-control" id="name" name="name" required>
+    <div class="col-md-4">      
+      <label for="name" class="form-label label">Nome Completo</label>
+      <div class="input-group">
+        <span class="input-group-text">
+          <i class="bi bi-person-circle"></i>
+        </span>
+        <input type="text" class="form-control" id="name" name="name"
+          value="{{ old('name', $editObjectInstance->name ?? '') }}" required placeholder="Fulano de tal" />
+      </div>
     </div>
     <div class="col-md-4">
       <label for="email" class="form-label label">E-mail</label>
@@ -22,26 +42,41 @@
     </div>
   </div><br />
   <div class="row">
-    <div class="col-md-4">
+    <div class="col-md-3">
       <label for="phone" class="form-label label">Telefone</label>
       <input type="text" class="form-control" id="phone" name="phone" required>
     </div>
-    <div class="col-md-4">
+    <div class="col-md-2">
       <label for="extension" class="form-label label">Ramal</label>
       <input type="text" class="form-control" id="extension" name="extension">
     </div>
-    <div class="col-md-4">
-      <label for="military" class="form-label label">Militar</label>
-      <select class="form-control" id="military" name="military" required>
-        <option value="1">Sim</option>
-        <option value="0">Não</option>
-      </select>
-    </div>
-  </div><br />
-  <div class="row">
-    <div class="col-md-4">
+
+    <div class="col-md-3">
       <label for="electronic_signature" class="form-label label">Assinatura Eletrônica</label>
       <input type="text" class="form-control" id="electronic_signature" name="electronic_signature" required>
     </div>
   </div><br />
 </x-AppComponents::form-template>
+
+@section('styles')
+  <style>
+    .label {
+      margin-bottom: 0px;
+      margin-left: 5px;
+    }
+
+    .form-checkbox:checked {
+      background-color: #4a5568;
+      /* Cor de tema escuro */
+      border-color: #4a5568;
+      /* Cor de tema escuro */
+    }
+
+    .form-checkbox {
+      background-color: #2d3748;
+      /* Cor de tema escuro */
+      border-color: #2d3748;
+      /* Cor de tema escuro */
+    }
+  </style>
+@endsection
