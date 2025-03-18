289e362f2fbccd6f3bacb095d8c9f4368adc3860
2S SIN Waldenylson <waldenylsonwpss@fab.mil.br>
2025-03-18 04:13:48 -0300

    feat: enhance key movements index view to display user military rank and service name, and format 'out' timestamp

--- resources/views/key_movements/index.blade.php
+++ resources/views/key_movements/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
   <div class="container">
     <h1>Chaves Movimentadas sem Devolução</h1>
-    
+
     <table class="table mt-3">
       <thead>
         <tr>
@@ -19,8 +19,8 @@
           <tr>
             <td>{{ $movement->key->number }}</td>
             <td>{{ $movement->harfStaff->name }}</td>
-            <td>{{ $movement->user->name }}</td>
-            <td>{{ $movement->out }}</td>
+            <td>{{ $movement->user->military_rank . ' ' . $movement->user->service_name }}</td>
+            <td>{{ \Carbon\Carbon::parse($movement->out)->format('d/m/Y H:i:s') }}</td>
             <td>
               <a href="{{ route('key_movements.edit', $movement->id) }}" class="btn btn-warning">Editar</a>
               <form action="{{ route('key_movements.destroy', $movement->id) }}" method="POST" style="display:inline;">
@@ -33,5 +33,9 @@
         @endforeach
       </tbody>
     </table>
+    <!-- Links de Paginação -->
+    <div class="d-flex justify-content-center">
+      {{ $movements->links('vendor.pagination.bootstrap-5') }}
+    </div>
   </div>
 @endsection
